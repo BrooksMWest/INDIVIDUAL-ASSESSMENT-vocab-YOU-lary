e7f8f5ca345c367cb2fe619ce8a9eea474848edf
Brooks West <brooksmwest@hotmail.com>
2024-06-08 10:04:47 -0500

    added buttons in the nav that are hooked up to content cards but i still need to get the right cards in there instead of all of them. this is close

--- pages/terms.js
+++ pages/terms.js
@@ -15,12 +15,13 @@ const showTerms = (array) => {
   let domString = '';
   array.forEach((item) => {
     domString += `
-      <div class="card">
-        <div class="card-body" style="height: 180px;">
+      <div class="card" style="width: 18rem;">
+        <div class="card-body" style="width: 18rem;">
           <h5 class="card-definition">${item.term}</h5>
+          <h3 class="card-category">Category: ${item.categoryId}</h3>
             <p class="card-text bold">${item.definition}</p>
             <hr>
-            <i class="btn btn-success fas fa-eye" id="view-term-btn--${item.firebaseKey}">View</i>
+
             <i id="edit-term-btn--${item.firebaseKey}" class="fas fa-edit btn btn-info">Edit</i>
             <i id="delete-term-btn--${item.firebaseKey}" class="btn btn-danger fas fa-trash-alt">Delete</i>
         </div>
